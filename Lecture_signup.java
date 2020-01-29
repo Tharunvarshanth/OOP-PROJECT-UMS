0cb3808f511897e3efcb942c27a4cd29b548e445
Tharun <49119576+Tharunvarshanth@users.noreply.github.com>
2020-01-29 21:25:40 +0530

    Update Lecture_signup.java

--- Lecture_signup.java
+++ Lecture_signup.java
@@ -4,16 +4,7 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import javax.swing.JOptionPane;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
-/**
- *
- * @author USER
- */
 public class Lecture_signup extends javax.swing.JFrame {
  
     String[] c=new String[4];
